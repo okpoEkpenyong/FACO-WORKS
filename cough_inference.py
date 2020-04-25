6b0fb706662540d598dc0f30e50525d7d5b76159
Francisco Tornay <echon.noun@gmail.com>
2020-04-25 18:20:20 +0200

    First commit

---
+++ cough_inference.py
@@ -0,0 +1,34 @@
+"""
+    Use:
+        inf = CoughInfer('model.pkl', 'directory)
+        img = arr2image(array)
+        category, image, probality = inf.predict(img) """
+
+from fastai import *
+import torch
+import numpy as np
+
+def arr2image(arr):
+    """ Converts numpy array to fastai Image with 3 channels """
+    # Drop last dimension
+    data = arr[:,:,0]
+    # Convert to tensor
+    data = torch.from_numpy(data)
+    # Stack 3 copies as new channels
+    data = torch.stack([data, data, data])
+    # return as image
+    return Image(data)
+
+class CoughInfer():
+    """ Class that carries out the inference """
+    def __init__(model, dir='.'):
+        """ Initialization, provide exported model file and dir """
+        self.learn = load_learner(dir, model):
+    
+    def predict(img):
+        """ Predicts from image """
+        pred = self.learn.predict(img)
+        # Returns tuple
+        # with category, image and probabilities of each cat
+        return pred
+
