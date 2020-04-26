39e3245e4d9fd5e006c390cd52e6ab92dcfd9251
Francisco Tornay <echon.noun@gmail.com>
2020-04-26 14:53:02 +0200

    Working on inference, added text for submission

--- cough_inference.py
+++ cough_inference.py
@@ -4,14 +4,15 @@
         img = arr2image(array)
         category, image, probality = inf.predict(img) """
 
-from fastai import *
+from fastai.vision.image import Image
+from fastai.basic_train import load_learner
 import torch
 import numpy as np
 
 def arr2image(arr):
     """ Converts numpy array to fastai Image with 3 channels """
-    # Drop last dimension
-    data = arr[:,:,0]
+    # Drop empty dimensions
+    data = np.squeeze(arr)
     # Convert to tensor
     data = torch.from_numpy(data)
     # Stack 3 copies as new channels
@@ -23,7 +24,7 @@ class CoughInfer():
     """ Class that carries out the inference """
     def __init__(model, dir='.'):
         """ Initialization, provide exported model file and dir """
-        self.learn = load_learner(dir, model):
+        self.learn = load_learner(dir, model)
     
     def predict(img):
         """ Predicts from image """
