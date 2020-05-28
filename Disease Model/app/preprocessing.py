03ff378921c916cb72ca75fe59e82161388729d0
neburseni <ragarciaruben@gmail.com>
2020-05-28 13:16:44 +0200

    Minor changes

--- Disease Model/app/preprocessing.py
+++ Disease Model/app/preprocessing.py
@@ -1,38 +1,20 @@
-import cv2
-import numpy as np
+import librosa as lb
+from librosa.display import specshow
+import matplotlib.pyplot as plt
 
-faceCascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_alt0.xml")
 
 
-# Read in and simultaneously preprocess video
-def read_video(path):
-    cap = cv2.VideoCapture(path)
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    video_frames = []
-    face_rects = ()
+def convert_to_spec_image(filename):
 
-    while cap.isOpened():
-        ret, img = cap.read()
-        if not ret:
-            break
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        roi_frame = img
+    y, sr = lb.load(filename)
+    # Plot signal in
+    plt.figure(figsize=(10, 3))
+    src_ft = lb.stft(y)
+    src_db = lb.amplitude_to_db(abs(src_ft))
+    specshow(src_db, sr=sr, x_axis='time', y_axis='hz')
+    plt.ylim(0, 5000)
+    filename = plt.savefig(filename+ '.png')
+    return filename
 
-        # Detect face
-        if len(video_frames) == 0:
-            face_rects = faceCascade.detectMultiScale(gray, 1.3, 5)
 
-        # Select ROI
-        if len(face_rects) > 0:
-            for (x, y, w, h) in face_rects:
-                roi_frame = img[y:y + h, x:x + w]
-            if roi_frame.size != img.size:
-                roi_frame = cv2.resize(roi_frame, (500, 500))
-                frame = np.ndarray(shape=roi_frame.shape, dtype="float")
-                frame[:] = roi_frame * (1. / 255)
-                video_frames.append(frame)
 
-    frame_ct = len(video_frames)
-    cap.release()
-
-    return video_frames, frame_ct, fps
